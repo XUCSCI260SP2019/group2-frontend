b31a12a42ccf5f50a19b52930b49e85d9e000cf6
Tyler <charuhast@xavier.edu>
2019-04-24 22:36:46 -0400

    Tyler Charuhas | CSCI 260 | calendar integration w login

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { OktaAuthModule } from '@okta/okta-angular';
+import { AuthInterceptor } from './shared/okta/auth.interceptor';
+import { HomeComponent } from './home/home.component';
+
+const config = {
+  issuer: 'https://dev-402557.okta.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oahpz08l2Xs85ut3356'
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    FormsModule,
+    OktaAuthModule.initAuth(config)
+  ],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
